6d0af5ddfa87b822eed2e9980d6e04cc90cc74ab
Kanchana Ranmuthu <kanchana.rdg@gmail.com>
2025-02-25 14:49:17 +0530

    first commit

---
+++ src/main/java/com/code/ghci/GhciApplication.java
@@ -0,0 +1,13 @@
+package com.code.ghci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/ghci/GhciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.ghci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
